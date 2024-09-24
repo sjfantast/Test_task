8bb61c50aebc368f1ea6c5ee09ca72f6f4c6a57c
sjfantast <sjfantast@gmail.com>
2024-09-24 14:05:23 +0500

    version_2

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-from math import trunc
+def main(example):
+    example = example.split()
 
-def main(ex):
-    example = check(ex)
+    if len(example) != 3:
+        return 'Введено невалидное значение'
 
-    if not (example):
-        return None
+    if example[1] not in ['+', '-', '*', '/']:
+        return 'Некорректная математическая операция'
+
+    try:
+        if not (1 <= int(example[0]) <= 10) or not (1 <= int(example[-1]) <= 10):
+            return 'Нарушено условие ввода чисел'
+    except Exception as ex:
+        return ex
 
     if example[1] == '+':
-        print(int(example[0]) + int(example[-1]))
+        return int(example[0]) + int(example[-1])
     elif example[1] == '*':
-        print(int(example[0]) * int(example[-1]))
+        return int(example[0]) * int(example[-1])
     elif example[1] == '-':
-        print(int(example[0]) - int(example[-1]))
+        return int(example[0]) - int(example[-1])
     else:
-        print(trunc(int(example[0]) / int(example[-1])))
+        return int(example[0]) // int(example[-1])
 
-def check(ex):
-    ex = ex.split()
-
-    if len(ex) != 3:
-        print('throws Exception')
-        return False
-
-    if not (ex[0].isdigit() and ex[-1].isdigit()):
-        print('throws Exception')
-        return False
-
-    if not (1 <= int(ex[0]) <= 10 and 1 <= int(ex[-1]) <= 10):
-        print("throws Exception")
-        return False
-
-    if ex[1] not in ['+', '-', '*', '/']:
-        print('throws Exception')
-        return False
-
-    return ex
-
-while True:
-    main(input())
